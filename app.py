c1226a0bae6b9ac1cf64e475db80c30f215006f5
swapnilajagunde2005-rgb <swapnilajagunde2005@gmail.com>
2026-08-28 22:16:17 +0530

    Add punkt_tab resource download to NLTK setup

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 @st.cache_resource
 def setup_nltk():
     nltk.download('punkt')
+    nltk.download('punkt_tab')
     nltk.download('stopwords')
     nltk.download('vader_lexicon')
 
